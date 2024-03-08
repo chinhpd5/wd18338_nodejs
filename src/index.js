67058ae1872942af4a810d47122857168908cbb4
chinhpd5 <chinhpd5@fpt.edu.vn>
2024-03-08 20:15:17 +0700

    last

--- src/index.js
+++ src/index.js
@@ -9,8 +9,8 @@ const port = 3000
 app.use(express.json());
 
 //views
-app.engine('handlebars', engine());
-app.set('view engine', 'handlebars');
+app.engine('hbs', engine({extname: '.hbs'}));
+app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname,'/views'));
 
 
